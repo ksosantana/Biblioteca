2d4f94ec6547ecc3f49a21c265f2b19ad1948cc3
ksosantana <kristhiansantana94@gmail.com>
2025-06-04 01:42:58 -0400

    adicionando index da inicial e dos ultimos com o app incompleto

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, send_from_directory, session, redirect, url_for
+
+app = Flask(__name__)
+app.secret_key = 'testes1'  # necessária para sessões
+
+# Rota para página inicial
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Rota para servir PDFs da pasta livros
+@app.route('/livros/<path:filename>')
+def livros(filename):
+    return send_from_directory('livros', filename)
+
+# Rota para abrir livro e guardar nos últimos abertos
+@app.route('/abrir/<filename>')
+def abrir_livro(filename):
+    ultimos = session.get('ultimos_livros', [])
+
+    if filename in ultimos:
+        ultimos.remove(filename)
+    ultimos.insert(0, filename)
+    ultimos = ultimos[:5]  # mantém só os 5 últimos
+    session['ultimos_livros'] = ultimos
+
+    return redirect(url_for('livros', filename=filename))
+
+# Rota para mostrar últimos livros abertos
+@app.route('/ultimos')
+def mostrar_ultimos():
+    ultimos = session.get('ultimos_livros', [])
+    return render_template('ultimos.html', ultimos=ultimos)
+
+if __name__ == '__main__':
+    app.run(debug=True)
