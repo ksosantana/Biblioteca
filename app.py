3447c2d796667e319f9d57f2d039d39965d52da0
ksosantana <kristhiansantana94@gmail.com>
2025-06-12 18:04:00 -0400

    adiciocionando imagem e livros d em pdf e a implementação do css

--- app.py
+++ app.py
@@ -1,33 +1,26 @@
 from flask import Flask, render_template, send_from_directory, session, redirect, url_for
 
 app = Flask(__name__, template_folder='telas', static_folder='organ')
+app.secret_key = '123456'  # Chave necessária para utilizar sessões
 
-app.secret_key = '123456'  # necessária para sessões
-
-# Rota para página inicial
 @app.route('/')
 def index():
     return render_template('index.html')
-
-# Rota para servir PDFs da pasta livros
 @app.route('/livros/<path:filename>')
 def livros(filename):
     return send_from_directory('livros', filename)
-
-# Rota para abrir livro e guardar nos últimos abertos
 @app.route('/abrir/<filename>')
 def abrir_livro(filename):
     ultimos = session.get('ultimos_livros', [])
 
+# ajuda do chat para naão ocasionar duplicagem de livros, com um erro que eu mesmo tive
     if filename in ultimos:
-        ultimos.remove(filename)
-    ultimos.insert(0, filename)
-    ultimos = ultimos[:5]  # mantém só os 5 últimos
-    session['ultimos_livros'] = ultimos
+        ultimos.remove(filename)  
+
+    ultimos.insert(0, filename)  # adiciona ao topo da lista
+    session['ultimos_livros'] = ultimos[:5]  # mantém apenas os 5 últimos
 
     return redirect(url_for('livros', filename=filename))
-
-# Rota para mostrar últimos livros abertos
 @app.route('/ultimos')
 def mostrar_ultimos():
     ultimos = session.get('ultimos_livros', [])
